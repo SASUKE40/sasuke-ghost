1aaacb60ad95fb6595b2d004dd579d38ee8dc9eb
SASUKE <sasuke688848.com>
2015-04-24 15:01:51 +0800

    Initial commit

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-  res.write('\
-    <style> body{ margin: 0px; } </style> \
-<iframe border="0" frameborder="0" marginwidth="0" marginheight="0" width="100%" height="100%" src="http://man.jcloud.com/appengine/jae/hello.html"> </iframe> \
-  ');
-  res.end();
-}).listen(process.env.PORT || 1337, null);
+// # Ghost bootloader
+// Orchestrates the loading of Ghost
+// When run from command line.
+
+var express,
+    ghost,
+    parentApp,
+    errors;
+
+// Make sure dependencies are installed and file system permissions are correct.
+require('./core/server/utils/startup-check').check();
+
+// Proceed with startup
+express = require('express');
+ghost = require('./core');
+errors = require('./core/server/errors');
+
+// Create our parent express app instance.
+parentApp = express();
+
+ghost().then(function (ghostServer) {
+    // Mount our ghost instance on our desired subdirectory path if it exists.
+    parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
+
+    // Let ghost handle starting our server instance.
+    ghostServer.start(parentApp);
+}).catch(function (err) {
+    errors.logErrorAndExit(err, err.context, err.help);
+});
